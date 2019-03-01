d6e42a92312319b3d821910ff4d7918176074e35
Phil Mike Jones <philmikejones@gmail.com>
2019-03-01 09:04:12 +0000

    update house prices script

--- src/process-house-price-thematic-map.py
+++ src/process-house-price-thematic-map.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import os
-import shutil
 import pandas as pd
 
 os.makedirs("data/external", exist_ok=True)
@@ -10,4 +9,6 @@ if not os.path.isfile("data/external/Average-prices-2016-07.csv"):
     file = "http://publicdata.landregistry.gov.uk/market-trend-data/house-price-index-data/Average-prices-2016-07.csv"
     file = pd.read_csv(file)
     file.to_csv("data/external/Average-prices-2016-07.csv")
-    print("Saved Average-prices-2016-07.csv")
+
+hp = pd.read_csv("data/external/Average-prices-2016-07.csv")
+hp.head()
