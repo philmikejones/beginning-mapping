66a435133796c38f67ebed2821a3463e0ab1cad1
Phil Mike Jones <philmikejones@gmail.com>
2019-03-25 21:53:37 +0000

    rename download-regions > download-shapefiles

--- src/download-shapefiles.py
+++ src/download-shapefiles.py
@@ -5,6 +5,7 @@ import requests
 import shutil
 
 os.makedirs("regions", exist_ok=True)
+os.makedirs("lad", exist_ok=True)
 
 if not os.path.isfile("regions/England_gor_2011.zip"):
     with open("regions/England_gor_2011.zip", "wb") as file:
@@ -41,3 +42,13 @@ if not os.path.isfile("regions/Wales_ol_2011.shp"):
     shutil.unpack_archive(
         "regions/Wales_ol_2011.zip", "regions"
     )
+
+if not os.path.isfile("lad/England_lad_2011.zip"):
+    with open("lad/England_lad_2011.zip", "wb") as file:
+        url = "https://borders.ukdataservice.ac.uk/ukborders/easy_download/prebuilt/shape/England_lad_2011.zip"
+        data = requests.get(url)
+        data = data.content
+        file.write(data)
+
+if not os.path.isfile("lad/England_lad_2011.shp"):
+    shutil.unpack_archive("lad/England_lad_2011.zip", "lad")
